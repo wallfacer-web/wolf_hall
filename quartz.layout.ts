5f04b28e30f8b47a0daa04903d0774c59a83a6bb
wallfacer-web <wallfacer-web@example.com>
2026-03-30 17:29:22 +0800

    Improve visible site navigation

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,10 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      folderDefaultState: "open",
+      useSavedState: false,
+    }),
   ],
   right: [
     Component.DesktopOnly(Component.Graph()),
@@ -62,7 +65,10 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      folderDefaultState: "open",
+      useSavedState: false,
+    }),
   ],
   right: [],
 }
